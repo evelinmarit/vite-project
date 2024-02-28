df712077e8990e8b285c8705d8f0f7d387d55a16
Evelin Maripuu <evelin.marit@gmail.com>
2024-02-28 22:43:25 +0200

    vite_project

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import myPix from './images/lion.jpg'
+document.getElementById('myPicture').src = myPix
+
+document.querySelector('#app').innerHTML = `
+
+    <h1>Hello Vite!</h1>
+      
+ `
